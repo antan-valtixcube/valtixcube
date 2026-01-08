ba124d0fde0d4d9525d558034e9ce6a43da92f6c
antancode <101625589+antancode@users.noreply.github.com>
2026-01-08 19:25:15 +0530

    topbar removed link

--- app/component/Header/Header.js
+++ app/component/Header/Header.js
@@ -11,8 +11,8 @@ export default function Header() {
       <div className={styles.topBar}>
         <div className={styles.frame}>
           <a href="tel:+919999999999" className={styles.item}>📞 +91 9876543210</a>
-          <span className={styles.divider}></span>
-          <a href="#" className={styles.icon}>🔗</a>
+          {/* <span className={styles.divider}></span>
+          <a href="#" className={styles.icon}>🔗</a> */}
         </div>
       </div>
 
